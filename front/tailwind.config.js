325e0cffa65c8b76b44096e4a76450ae624ffe6c
Flew2k <dulguunpuntsag@gmail.com>
2023-02-13 19:05:23 +0800

    implemented header

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Mulish": ['Mulish'],
+      },
+      fontSize: {
+        ht: ["16px", "28px"],
+        bt: ["16px", '24px'],
+        mt: ["48px", '60.24px'],
+      }
+    },
   },
   plugins: [],
 }
