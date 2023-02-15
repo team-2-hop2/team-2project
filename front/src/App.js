514164051bb21e58939bd81b4496c0455dfe6888
Buyantogtokh <HOP2-Buyantogtokh@Iveelts-iMac.local>
2023-02-15 17:28:16 +0800

    blog

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+<<<<<<< Updated upstream
+=======
+import { Header } from './components/header';
+import { Blog } from './components/blog';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
+=======
+      <Header/>
+>>>>>>> Stashed changes
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Blog/>
       </header>
     </div>
   );
