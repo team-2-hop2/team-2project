00763abe208e37909c83566909c7e49000a9aede
Buyantogtokh <HOP2-Buyantogtokh@Iveelts-iMac.local>
2023-02-15 18:17:21 +0800

    blog2

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,9 @@
 
 import './App.css';
-<<<<<<< Updated upstream
-=======
-import { Header } from './components/header';
 import { Blog } from './components/blog';
->>>>>>> Stashed changes
-
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-=======
-      <Header/>
->>>>>>> Stashed changes
       <header className="App-header">
         <Blog/>
       </header>
