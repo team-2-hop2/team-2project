b6225ada74d82f5ea042c48c90d1ab0f09d95cb5
Buyantogtokh <HOP2-Buyantogtokh@Iveelts-iMac.local>
2023-02-17 19:23:24 +0800

    fixed blogpost

--- front/src/pages/BlogPost.jsx
+++ front/src/pages/BlogPost.jsx
@@ -4,12 +4,17 @@ import {Blog} from '../components/blog'
 export const BlogPost = () => {
   return (
   <div className=" w-screen h-auto flex flex-col items-center justify-center bg-[#F5F5F5]">
-    <div className="h-auto w-10/12 flex  flex-col mt-44 ">
-        <p className="text-black text-5xl font-Mulish">Blog posts</p>
-        <p className="text-[#6D7D8B] text-lg font-Mulish ">Our late updates and blogs about managing your team</p>
-        <div className="inline-grid grid-cols-3 gap-10">
-            <Blog/>
+    <div className="h-auto w-10/12 flex flex-col mt-44 gap-10 items-center">
+        <p className="text-black text-5xl font-Mulish w-full flex justify-start">Blog posts</p>
+        <p className="text-[#6D7D8B] text-lg font-Mulish  w-full flex justify-start">Our late updates and blogs about managing your team</p>
+        <div className="flex flex-wrap w-10/12 justify-center gap-10">
+            <Blog title={'10 secret tips for managing a team remotely'} comment={"There are times when our work impacts us deepl"} firstname={"Bessie"} lastname={"Cooper"} date={"2nd January,2022"}/>
             <Blog/>  
+            <Blog/>
+            <Blog/>
+            <Blog/>
+            <Blog/>
+            <Blog/>
             <Blog/>
             <Blog/>
         </div>
